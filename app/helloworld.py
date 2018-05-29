e8e6787e277219537340f93de625f56af71eecaa
cocopop1106 <zhzh1300>
2018-05-29 17:51:17 +0900

    zcp-webhook

---
+++ app/helloworld.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import json
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+if __name__ == "__main__":
+    app.run()
